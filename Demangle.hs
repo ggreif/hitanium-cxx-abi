dbe80c5480cc33e88b574b6fa3ef058b9b3f7356
Gabor Greif <ggreif@gmail.com>
2014-02-27 20:03:02 +0100

    add a mildly interesting testcase

--- Demangle.hs
+++ Demangle.hs
@@ -3,3 +3,7 @@ module Demagle () where
 demangle' assocs ('_':'_':'Z':more) = demangle' assocs ('_':'Z':more)
 demangle' assocs ('_':'Z':more) = "dunno"
 
+
+-- Constructor of Disco<__typeof__(&foo::bar), &foo::bar>
+--
+t1 = "__ZN5DiscoIM3fooFivEXadL_ZNS0_3barEvEEEC1Ev"
